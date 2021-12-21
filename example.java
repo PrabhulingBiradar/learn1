81a4b26bb62e07d9fa40214e286a7db9676bc911
PrabhulingBiradar <prabulingabiradar537.xworkz@gmail.com>
2021-12-21 10:40:18 +0530

    learning example

---
+++ example.java
@@ -0,0 +1,20 @@
+class example
+{
+public static void main(String[] arg)
+
+{
+int height[]={156,157,159,131,135,139,161,171,175,177};
+ System.out.println(height[0]);
+System.out.println(height[1]);
+System.out.println(height[2]);
+System.out.println(height[3]);
+System.out.println(height[4]);
+System.out.println(height[5]);
+System.out.println(height[6]);
+System.out.println(height[7]);
+System.out.println(height[8]);
+System.out.println(height[9]);
+
+}
+
+}
